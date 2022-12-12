2d8ec0cfac25fd871b29bc83fbb79a76db510724
IkigaiRamen <55280193+IkigaiRamen@users.noreply.github.com>
2022-12-12 22:41:26 +0100

    more paths

--- src/app/universite/universite-routing.module.ts
+++ src/app/universite/universite-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddContratComponent } from 'app/contrat/add-contrat/add-contrat.component';
+import { ListeContratComponent } from 'app/contrat/liste-contrat/liste-contrat.component';
+import { UpdateContratComponent } from 'app/contrat/update-contrat/update-contrat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:ListeContratComponent},
+  {path:'updateContrat/:id',component:UpdateContratComponent},
+  {path:'addContrat',component:AddContratComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
